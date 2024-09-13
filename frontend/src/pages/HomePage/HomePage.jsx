f6546a8de2857dc88f919ddf027bfb386cb881f8
jomir <jomir.uddin112@gmail.com>
2024-09-13 13:44:09 -0400

    Landing page almosst complete/img will be aded later

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,14 @@
+import styles from './HomePage.module.css';
+
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <h1 className={styles.title}>AppTrackr</h1>
+      <h2 className={styles.subtitle}>Track Your Job Applications</h2>
+      <img src="" alt="" />
+      <p>Stay Organized in Your Job Search</p>
+      <p>Track your job applications effortlessly with AppTrackr, your all-in-one job search KanBan board.</p>
+      <button className={styles.button} >Sign up for free!</button>
+    </>
+  );
 }
